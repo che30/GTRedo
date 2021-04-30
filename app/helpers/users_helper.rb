840e952f3e17c013e3486d1c59f9dd66089da6b4
che <che.blancoo58@gmail.com>
2021-04-30 21:31:58 +0100

    Add logout functionality

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,14 @@
 module UsersHelper
   def user_avatar
-    "fa fa-user-circle"
+    'fa fa-user-circle'
   end
+
+  def image_available?
+    if current_user.avatar
+      'd-none'
+    else
+      'd-block'
+    end
+  end
+
 end
