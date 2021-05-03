a042a229b25b78ff0938e8cd7d3008a6231a2aca
che <che.blancoo58@gmail.com>
2021-05-03 11:31:56 +0100

    Add Transaction index page and some styles

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,7 @@
 class Group < ApplicationRecord
   belongs_to :user
   has_many :transactions, dependent: :destroy
+  has_many_attached :avatar,dependent: :destroy
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :icon, presence: true
 end
