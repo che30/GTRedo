537b379f7374d8c85734789d173cb97cd0b5cbb5
che <che.blancoo58@gmail.com>
2021-05-06 19:16:04 +0100

    Add new path

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
   get '/home', to: 'home#homepage'
 
   resources :users,only: [:edit, :update, :show, :destory,:create]
-  resources :transactions,only: [:show,:index,:create]
-  resources :groups,only: [:show, :index,:create]
+  resources :transactions,only: [:new,:show,:index,:create]
+  resources :groups, only: [:new,:show, :index,:create]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
