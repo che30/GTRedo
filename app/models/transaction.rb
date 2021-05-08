659c4cc764f50b74177d47d1a71b4caa32aa8050
che <che.blancoo58@gmail.com>
2021-05-08 13:32:55 +0100

    Fix transaction show page

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,6 @@
 class Transaction < ApplicationRecord
   belongs_to :user
   belongs_to :group, optional: true
-  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :name, presence: true
   validates :amount, presence: true
 end
