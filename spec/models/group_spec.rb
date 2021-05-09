d37b565512af2fd6aa498a7c28819594eaa6c50a
che <che.blancoo58@gmail.com>
2021-05-09 15:06:29 +0100

    fix linters and rubocop errors

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,13 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  let(:user) { User.create(name: 'blanco',email: 'two@user.com',password: '123456') }
+  let(:user) { User.create(name: 'blanco', email: 'two@user.com', password: '123456') }
   let(:group) { user.groups.create(name: 'abc', avatar: 'app/assets/images/login.png') }
   context 'testing group controller method and models' do
-     it ' saves an group ' do
+    it ' saves an group ' do
       valid_user = User.new(name: 'User01', email: 'two@user.com', password: '123456')
-    valid_user.avatar.attach(io: File.open('app/assets/images/login.png'), filename: "login.png", content_type: "images/png")
-    expect(valid_user.avatar.attached?).to be(true)
-     end
+      valid_user.avatar.attach(io: File.open('app/assets/images/login.png'), filename: 'login.png',
+                               content_type: 'images/png')
+      expect(valid_user.avatar.attached?).to be(true)
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,21 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
   before(:each) do
     @user1 = User.new(name: 'User01', email: 'two@user.com', password: '123456')
   end
   context 'validation tests' do
     it ' ensures name not blank' do
-      user = User.new(name: '',email: 'two@user.com',password: '123456').save
+      user = User.new(name: '', email: 'two@user.com', password: '123456').save
       expect(user).to eql(false)
-      
     end
     it 'test if a user is valid ' do
-      expect(@user1).to be_valid 
+      expect(@user1).to be_valid
     end
     it 'ensures length of username should at least 3' do
-      user = User.new(email: 'two@user.com',password: '123456')
+      user = User.new(email: 'two@user.com', password: '123456')
       expect(user.valid?).to eql(false)
     end
     it 'can create a new user' do
