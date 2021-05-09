c537ee07a495568a55771258b6504d874cc92cb9
che <che.blancoo58@gmail.com>
2021-05-09 14:48:02 +0100

    Add more rspec tests

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -2,26 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Group, type: :model do
   let(:user) { User.create(name: 'blanco',email: 'two@user.com',password: '123456') }
-  let(:group) { user.groups.create(name: 'abc', avatar: '') }
+  let(:group) { user.groups.create(name: 'abc', avatar: 'app/assets/images/login.png') }
   context 'testing group controller method and models' do
-    it 'expects a new group to created successfuly' do
-      expect(group.valid?).to eq(true)
-    end
-    # it "doesn't create a group with an empty name" do
-    #   group = Group.create(name: '')
-    #   expect(group.valid?).to eq false
-    # end
-    # it 'creates a group only if the name is unique' do
-    #   group2 = Group.create(name: group.name.to_s)
-    #   expect(group2.valid?).to eq false
-    # end
-    # it 'creates a group only if the name is unique' do
-    #   group2 = Group.create(name: group.name.to_s)
-    #   expect(group2.valid?).to eq false
-    # end
-    # it 'creates a group not needing an icon' do
-    #   group1 = user.groups.create(name: 'abc', icon: '')
-    #   expect(group1.icon.nil? & group1.valid?).to eq false
-    # end
+     it ' saves an group ' do
+      valid_user = User.new(name: 'User01', email: 'two@user.com', password: '123456')
+    valid_user.avatar.attach(io: File.open('app/assets/images/login.png'), filename: "login.png", content_type: "images/png")
+    expect(valid_user.avatar.attached?).to be(true)
+     end
   end
 end
